18119abf4a90009bd079af31b4dcc39be0c0b358
rinaa <rinaa@LAPTOP-5GLJIRJF>
2016-10-08 14:53:46 +0200

    Change to connection

---
+++ Controllers/ConnectionsController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AzureApiApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AzureApiApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class ConnectionsController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public async Task<IList<SectionResult>> Get()
+        {
+            using (var Client = new System.Net.Http.HttpClient())
+            {
+                // ReSharper disable once InconsistentNaming
+                var response =
+                    await Client.GetAsync(@"http://transport.opendata.ch/v1/connections?from=Lausanne, Dranse&to=Nyon");
+                if (response != null && response.IsSuccessStatusCode)
+                {
+                    var ResponseBody = await response.Content.ReadAsStringAsync();
+                    try
+                    {
+                        var ObjResponse = JsonConvert.DeserializeObject<Connections>(ResponseBody);
+                        if (ObjResponse != null)
+                        {
+                            var ConnectionList = ObjResponse.ConnectionsList;
+
+                            var SectionResultList = new List<SectionResult>();
+
+                            var ConnectionNumber = 1;
+                            foreach (var Connection in ConnectionList)
+                            {
+                                var SectionNumber = 1;
+                                foreach (var Section in Connection.Sections)
+                                {
+                                    SectionResultList.Add(new SectionResult
+                                    {
+                                        ConnectionNumber = ConnectionNumber,
+                                        SectionNumer = SectionNumber,
+                                        JourneyName = Section.Journey != null ? Section.Journey.Name : "Walk",
+                                        DepartureStation = Section.Departure.Station.Name,
+                                        ArrivalStation = Section.Arrival.Station.Name,
+                                        Duration = Section.Arrival.ArrivalTime.Subtract(Section.Departure.DepartureTime)
+                                    });
+                                    SectionNumber++;
+                                }
+                                ConnectionNumber++;
+                            }
+
+                            return SectionResultList;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        // in case an error occured, we do not return any data
+                    }
+                }
+            }
+            return new List<SectionResult>();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+
+    public class SectionResult
+    {
+        public int ConnectionNumber { get; set; }
+        public int SectionNumer { get; set; }
+        public string JourneyName { get; set; }
+        public string DepartureStation { get; set; }
+        public string ArrivalStation { get; set; }
+        public TimeSpan Duration { get; set; }
+    }
+}
+
---
+++ Models/Connections.cs
@@ -0,0 +1,403 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace AzureApiApp.Models
+{
+    public class Coordinate
+    {
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("x")]
+        public double X { get; set; }
+
+        [JsonProperty("y")]
+        public double Y { get; set; }
+    }
+
+    public class Station
+    {
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("score")]
+        public int? Score { get; set; }
+
+        [JsonProperty("coordinate")]
+        public Coordinate Coordinate { get; set; }
+
+        [JsonProperty("distance")]
+        public object Distance { get; set; }
+    }
+
+    public class Prognosis
+    {
+
+        [JsonProperty("platform")]
+        public string Platform { get; set; }
+
+        [JsonProperty("arrival")]
+        public object Arrival { get; set; }
+
+        [JsonProperty("departure")]
+        public DateTime? Departure { get; set; }
+
+        [JsonProperty("capacity1st")]
+        public int? Capacity1St { get; set; }
+
+        [JsonProperty("capacity2nd")]
+        public int? Capacity2Nd { get; set; }
+    }
+
+    public class Location
+    {
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("score")]
+        public int? Score { get; set; }
+
+        [JsonProperty("coordinate")]
+        public Coordinate Coordinate { get; set; }
+
+        [JsonProperty("distance")]
+        public object Distance { get; set; }
+    }
+
+    public partial class From
+    {
+
+        [JsonProperty("station")]
+        public Station Station { get; set; }
+
+        [JsonProperty("arrival")]
+        public object Arrival { get; set; }
+
+        [JsonProperty("arrivalTimestamp")]
+        public int? ArrivalTimestamp { get; set; }
+
+        [JsonProperty("departure")]
+        public DateTime? Departure { get; set; }
+
+        [JsonProperty("departureTimestamp")]
+        public int DepartureTimestamp { get; set; }
+
+        [JsonProperty("delay")]
+        public int? Delay { get; set; }
+
+        [JsonProperty("platform")]
+        public string Platform { get; set; }
+
+        [JsonProperty("prognosis")]
+        public Prognosis Prognosis { get; set; }
+
+        [JsonProperty("realtimeAvailability")]
+        public string RealtimeAvailability { get; set; }
+
+        [JsonProperty("location")]
+        public Location Location { get; set; }
+    }
+
+    public partial class From
+    {
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("score")]
+        public int? Score { get; set; }
+
+        [JsonProperty("coordinate")]
+        public Coordinate Coordinate { get; set; }
+
+        [JsonProperty("distance")]
+        public object Distance { get; set; }
+    }
+
+    public partial class To
+    {
+
+        [JsonProperty("station")]
+        public Station Station { get; set; }
+
+        [JsonProperty("arrival")]
+        public DateTime Arrival { get; set; }
+
+        [JsonProperty("arrivalTimestamp")]
+        public int? ArrivalTimestamp { get; set; }
+
+        [JsonProperty("departure")]
+        public object Departure { get; set; }
+
+        [JsonProperty("departureTimestamp")]
+        public object DepartureTimestamp { get; set; }
+
+        [JsonProperty("delay")]
+        public int? Delay { get; set; }
+
+        [JsonProperty("platform")]
+        public string Platform { get; set; }
+
+        [JsonProperty("prognosis")]
+        public Prognosis Prognosis { get; set; }
+
+        [JsonProperty("realtimeAvailability")]
+        public string RealtimeAvailability { get; set; }
+
+        [JsonProperty("location")]
+        public Location Location { get; set; }
+    }
+
+    public class Service
+    {
+
+        [JsonProperty("regular")]
+        public string Regular { get; set; }
+
+        [JsonProperty("irregular")]
+        public string Irregular { get; set; }
+    }
+
+    public class PassList
+    {
+
+        [JsonProperty("station")]
+        public Station Station { get; set; }
+
+        [JsonProperty("arrival")]
+        public DateTime? Arrival { get; set; }
+
+        [JsonProperty("arrivalTimestamp")]
+        public int? ArrivalTimestamp { get; set; }
+
+        [JsonProperty("departure")]
+        public DateTime? Departure { get; set; }
+
+        [JsonProperty("departureTimestamp")]
+        public int? DepartureTimestamp { get; set; }
+
+        [JsonProperty("delay")]
+        public int? Delay { get; set; }
+
+        [JsonProperty("platform")]
+        public string Platform { get; set; }
+
+        [JsonProperty("prognosis")]
+        public Prognosis Prognosis { get; set; }
+
+        [JsonProperty("realtimeAvailability")]
+        public string RealtimeAvailability { get; set; }
+
+        [JsonProperty("location")]
+        public Location Location { get; set; }
+    }
+
+    public class Journey
+    {
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("category")]
+        public string Category { get; set; }
+
+        [JsonProperty("subcategory")]
+        public object Subcategory { get; set; }
+
+        [JsonProperty("categoryCode")]
+        public int CategoryCode { get; set; }
+
+        [JsonProperty("number")]
+        public string Number { get; set; }
+
+        [JsonProperty("operator")]
+        public string Operator { get; set; }
+
+        [JsonProperty("to")]
+        public string To { get; set; }
+
+        [JsonProperty("passList")]
+        public IList<PassList> PassList { get; set; }
+
+        [JsonProperty("capacity1st")]
+        public int? Capacity1St { get; set; }
+
+        [JsonProperty("capacity2nd")]
+        public int? Capacity2Nd { get; set; }
+    }
+
+    public class Departure
+    {
+
+        [JsonProperty("station")]
+        public Station Station { get; set; }
+
+        [JsonProperty("arrival")]
+        public Arrival Arrival { get; set; }
+
+        [JsonProperty("arrivalTimestamp")]
+        public int? ArrivalTimestamp { get; set; }
+
+        [JsonProperty("departure")]
+        public DateTime DepartureTime { get; set; }
+
+        [JsonProperty("departureTimestamp")]
+        public int DepartureTimestamp { get; set; }
+
+        [JsonProperty("delay")]
+        public int? Delay { get; set; }
+
+        [JsonProperty("platform")]
+        public string Platform { get; set; }
+
+        [JsonProperty("prognosis")]
+        public Prognosis Prognosis { get; set; }
+
+        [JsonProperty("realtimeAvailability")]
+        public string RealtimeAvailability { get; set; }
+
+        [JsonProperty("location")]
+        public Location Location { get; set; }
+    }
+
+    public class Arrival
+    {
+
+        [JsonProperty("station")]
+        public Station Station { get; set; }
+
+        [JsonProperty("arrival")]
+        public DateTime ArrivalTime { get; set; }
+
+        [JsonProperty("arrivalTimestamp")]
+        public int? ArrivalTimestamp { get; set; }
+
+        [JsonProperty("departure")]
+        public object Departure { get; set; }
+
+        [JsonProperty("departureTimestamp")]
+        public object DepartureTimestamp { get; set; }
+
+        [JsonProperty("delay")]
+        public int? Delay { get; set; }
+
+        [JsonProperty("platform")]
+        public string Platform { get; set; }
+
+        [JsonProperty("prognosis")]
+        public Prognosis Prognosis { get; set; }
+
+        [JsonProperty("realtimeAvailability")]
+        public string RealtimeAvailability { get; set; }
+
+        [JsonProperty("location")]
+        public Location Location { get; set; }
+    }
+
+    public class Section
+    {
+
+        [JsonProperty("journey")]
+        public Journey Journey { get; set; }
+
+        [JsonProperty("walk")]
+        public object Walk { get; set; }
+
+        [JsonProperty("departure")]
+        public Departure Departure { get; set; }
+
+        [JsonProperty("arrival")]
+        public Arrival Arrival { get; set; }
+    }
+
+    public class Connection
+    {
+
+        [JsonProperty("from")]
+        public From From { get; set; }
+
+        [JsonProperty("to")]
+        public To To { get; set; }
+
+        [JsonProperty("duration")]
+        public string Duration { get; set; }
+
+        [JsonProperty("transfers")]
+        public int Transfers { get; set; }
+
+        [JsonProperty("service")]
+        public Service Service { get; set; }
+
+        [JsonProperty("products")]
+        public IList<string> Products { get; set; }
+
+        [JsonProperty("capacity1st")]
+        public int? Capacity1St { get; set; }
+
+        [JsonProperty("capacity2nd")]
+        public int? Capacity2Nd { get; set; }
+
+        [JsonProperty("sections")]
+        public IList<Section> Sections { get; set; }
+    }
+
+    public partial class To
+    {
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("score")]
+        public int? Score { get; set; }
+
+        [JsonProperty("coordinate")]
+        public Coordinate Coordinate { get; set; }
+
+        [JsonProperty("distance")]
+        public object Distance { get; set; }
+    }
+
+    public class Stations
+    {
+
+        [JsonProperty("from")]
+        public IList<From> From { get; set; }
+
+        [JsonProperty("to")]
+        public IList<To> To { get; set; }
+    }
+
+    public class Connections
+    {
+
+        [JsonProperty("connections")]
+        public IList<Connection> ConnectionsList { get; set; }
+
+        [JsonProperty("from")]
+        public From From { get; set; }
+
+        [JsonProperty("to")]
+        public To To { get; set; }
+
+        [JsonProperty("stations")]
+        public Stations Stations { get; set; }
+    }
+
+
+}
